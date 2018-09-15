4fe4f816e6c5513ab68c4c9c60b2e2fc2eac0889
niujunfeng <junfeng279@163.com>
2018-09-16 00:29:39 +0800

    添加分页组件

---
+++ blogui/src/components/common/page.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav aria-label="Page navigation">
+    <ul class="pagination pagination-lg pageClass">
+      <li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
+      <li><a href="#">1</a></li>
+      <li><a href="#">2</a></li>
+      <li><a href="#">3</a></li>
+      <li><a href="#">4</a></li>
+      <li><a href="#">5</a></li>
+      <li class="next"><a href="#">Newer <span aria-hidden="true">&rarr;</span></a></li>
+    </ul>
+  </nav>
+</template>
+<script>
+
+</script>
+<style>
+  .pageClass{
+    margin-left: 23%;
+  }
+</style>
